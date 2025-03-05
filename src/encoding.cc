4fb1894c4375cf0aa18069037d04d250bddb6a4c
Akari <jvjdev@gmail.com>
2025-03-05 15:13:58 +0800

    fix encoding

--- src/encoding.cc
+++ src/encoding.cc
@@ -7,20 +7,11 @@
 #include <spdlog/spdlog.h>
 
 std::string kbswitch::wstr2con(const std::wstring& s) {
-  auto output_encoding = GetConsoleOutputCP();
-  int output_size
-    = WideCharToMultiByte(output_encoding, 0, s.c_str(), -1, nullptr, 0, nullptr, nullptr);
+  int output_size = WideCharToMultiByte(CP_UTF8, 0, s.c_str(), -1, nullptr, 0, nullptr, nullptr);
   // count includes the null terminator, but we don't want it
   std::string result(output_size, '\0');
-  int actual_size = WideCharToMultiByte(
-    output_encoding,
-    0,
-    s.c_str(),
-    -1,
-    result.data(),
-    output_size,
-    nullptr,
-    nullptr);
+  int actual_size
+    = WideCharToMultiByte(CP_UTF8, 0, s.c_str(), -1, result.data(), output_size, nullptr, nullptr);
   if (actual_size < output_size && actual_size == 0) {
     spdlog::error("WideCharToMultiByte failed: {}", GetLastError());
     return "<ERROR>";
@@ -30,12 +21,10 @@ std::string kbswitch::wstr2con(const std::wstring& s) {
 }
 
 std::wstring kbswitch::con2wstr(const std::string& s) {
-  auto input_encoding = GetConsoleCP();
-  int output_size = MultiByteToWideChar(input_encoding, 0, s.c_str(), -1, nullptr, 0);
+  int output_size = MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, nullptr, 0);
   // count includes the null terminator, but we don't want it
   std::wstring result(output_size, L'\0');
-  int actual_size
-    = MultiByteToWideChar(input_encoding, 0, s.c_str(), -1, result.data(), output_size);
+  int actual_size = MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, result.data(), output_size);
   if (actual_size < output_size && actual_size == 0) {
     spdlog::error("MultiByteToWideChar failed: {}", GetLastError());
     return L"<ERROR>";
